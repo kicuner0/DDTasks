4824b126d615db409a3fc03fb38b5ae58719252b
Pavlov Kirill <90990452+kicuner0@users.noreply.github.com>
2022-09-18 20:46:50 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.RegularExpressions;
+
+var fileName = "read.txt";
+
+var wordCounts = new Dictionary<string, int>();
+
+if (File.Exists(fileName))
+{
+    using (StreamReader fs = new StreamReader(fileName))
+    {
+        while (true)
+        {
+            var temp = fs.ReadLine();
+
+            if (temp == null)
+            {
+                break;
+            }
+
+            temp = Regex.Replace(temp, "(?i)[^А-ЯЁA-Z]", " ");
+
+            var words = temp.ToLower().Split(' ');
+
+            foreach (var word in words)
+            {
+                if (word != "")
+                {
+                    if (wordCounts.ContainsKey(word))
+                    {
+                        wordCounts[word]++;
+                    }
+                    else
+                    {
+                        wordCounts.Add(word, 1);
+                    }
+                }
+            }
+        }
+    }
+
+    using (StreamWriter sw = new StreamWriter("write.txt"))
+    {
+        foreach (var wordCount in wordCounts.OrderByDescending(wordCount => wordCount.Value))
+        {
+            sw.WriteLine($"{wordCount.Key} {wordCount.Value}");
+        }
+    }
+}
